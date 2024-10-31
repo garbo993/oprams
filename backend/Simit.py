ef4f962db9dfe1d0218db3c8a30ec9a9d1568134
garbo993 <gdhernandezv@udistrital.edu.co>
2024-10-31 12:05:03 -0500

    funcionalidad json añádida y corecciones menores

--- backend/Simit.py
+++ backend/Simit.py
@@ -7,8 +7,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import json
 
 #############################################
+
+def convertir_a_Json(data):
+    if "no tienes comparendos ni multas registradas en simit" in data.lower():
+        info_dict = {"comparendos": "No tienes comparendos ni multas registradas en Simit"}
+        
+    else:
+
+        lines = data.strip().split("\n")[1:]
+        # Creamos un diccionario a partir de las líneas
+        info_dict = {}
+        for line in lines:
+            key, value = line.split(": ")
+            info_dict[key.strip()] = value.strip()
+
+    # Convertimos el diccionario a JSON
+    info_json = json.dumps(info_dict, ensure_ascii=False, indent=4)
+    print(info_json)
+
+###################################################
+
 #opciones de navegacion 
 def ConsultaSimit():
     service=Service(ChromeDriverManager().install())
@@ -21,25 +42,27 @@ def ConsultaSimit():
     option.add_argument("--disable-extensions")
     driver= Chrome(service=service, options = option)
     #inicializar navegador
-    placa = 'abc123' #placa a ingresar 
+    placa = 'gqt188' #placa a ingresar 
     website = "https://www.fcm.org.co/simit/#/estado-cuenta?numDocPlacaProp="+ placa # url + la placa a imgresar
     driver.get(website)
-
-    opcion = 0 
+    # try catch dependiendo de si tiene o no comparendos 
     try:
     # realiza la consulta buscando si no tiene comparendo con un tiempo de espera de 20 segundos 
-        respuesta = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[9]/div/div/div/div[1]/div/div[1]/div/div/div/h3"))) # validacion si la placa no tiene comparendos
-        print(respuesta.text) # respuesta del servicio 
+        respuesta = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[9]/div/div/div/div[1]/div/div[1]/div/div/div/h3"))).text # validacion si la placa no tiene comparendos
+        print(respuesta) # respuesta del servicio 
 
     except:
         # realiza la consulta buscando si tiene comparendos con un tiempo de espera de 20 segundos 
-        respuesta = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[9]/div/div/div/div[1]/div/div[1]/div[1]/div[1]"))) # validacion si la placa tiene comparendos
-        print(respuesta.text) # respuesta del servicio 
-    
+        respuesta = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH,"/html/body/div[9]/div/div/div/div[1]/div/div[1]/div[1]/div[1]"))).text # validacion si la placa tiene comparendos
+        print(respuesta) # respuesta del servicio 
+        
+    convertir_a_Json(respuesta)
     time.sleep(5)
     driver.quit()
 
+
 ConsultaSimit()
+
 
 
 #if __name__ =="__main__ ":
